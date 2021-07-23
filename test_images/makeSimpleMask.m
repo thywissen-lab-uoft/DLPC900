4fdd4f7633cd07a4df62a1814ae3a27d395ec25d
Cora <63414958+cjfujiwara@users.noreply.github.com>
2021-07-23 13:39:36 -0400

    be

--- test_images/makeSimpleMask.m
+++ test_images/makeSimpleMask.m
@@ -9,7 +9,7 @@ Y=1:1080;
 mask=zeros(1080,1920);
 
 % Circle Radius
-R=20;
+R=50;
 
 Yc=1080/2;
 Xc=1920/2;
@@ -24,7 +24,7 @@ pos=[...
     -1 0;
     1 0;
     2 0];
-pos=pos*100;
+pos=pos*200;
 
 foo=@(x,y,xC,yC) ((x-xC).^2+(y-yC).^2)<=R.^2;
 
@@ -34,7 +34,7 @@ for kk=1:size(pos,1)
 end
 mask_L=logical(mask);
 imwrite(mask_L, 'test.tif','tif','Compression','none');
-imwrite(mask, 'test_rle.tif','tif','Compression','rle');
+% imwrite(mask, 'test_rle.tif','tif','Compression','rle');
 
     
     

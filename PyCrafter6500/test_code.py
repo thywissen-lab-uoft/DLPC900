09022cb14578ea45959851ad9585ec067c7918df
thywissen-lab-uoft <52182330+thywissen-lab-uoft@users.noreply.github.com>
2021-07-23 12:29:45 -0400

    test images and edits

--- PyCrafter6500/test_code.py
+++ PyCrafter6500/test_code.py
@@ -2,22 +2,24 @@ import pycrafter6500
 import numpy
 import PIL.Image
 
-
+# Create the bmp image map
 images=[]
-
 images.append((numpy.asarray(PIL.Image.open("testimage.tif"))//129))
 
+# Open the USB connection to the DMD
 dlp=pycrafter6500.dmd()
 
+# Stop DMD
 dlp.stopsequence()
 
+# Patter on the fly mode
 dlp.changemode(3)
 
-exposure=[1000000]*30
-dark_time=[0]*30
-trigger_in=[False]*30
-trigger_out=[1]*30
+exposure=[500*10**3] # Exposure time in us
+dark_time=[0]*30    # Dak time in us
+trigger_in=[True]   # Whether to use an input trigger
+trigger_out=[False] # Whether to enable the output trigger
+repititions=1       # Number of times to repeat 0 is infinite
 
-dlp.defsequence(images,exposure,trigger_in,dark_time,trigger_out,0)
-
+dlp.defsequence(images,exposure,trigger_in,dark_time,trigger_out,repititions)
 dlp.startsequence()

38f76bc4549582c9ca3cd406044ef6cba3d0f288
Cora <63414958+cjfujiwara@users.noreply.github.com>
2021-07-23 13:29:32 -0400

    simple masks

---
+++ test_images/makeSimpleMask.m
@@ -0,0 +1,41 @@
+function makeSimpleMask
+
+fname='simple.tif';
+
+X=1:1920;
+Y=1:1080;
+
+[xx,yy]=meshgrid(X,Y);
+mask=zeros(1080,1920);
+
+% Circle Radius
+R=20;
+
+Yc=1080/2;
+Xc=1920/2;
+
+pos=[...
+    0 -2;
+    0 -1
+    0 0;
+    0 1;
+    0 2;
+    -2 0;
+    -1 0;
+    1 0;
+    2 0];
+pos=pos*100;
+
+foo=@(x,y,xC,yC) ((x-xC).^2+(y-yC).^2)<=R.^2;
+
+for kk=1:size(pos,1)
+   mask=mask+foo(xx,yy,Xc+pos(kk,1),Yc+pos(kk,2))   ;
+    
+end
+mask=logical(mask);
+imwrite(mask, 'test.tif','tif');
+    
+    
+
+end
+
